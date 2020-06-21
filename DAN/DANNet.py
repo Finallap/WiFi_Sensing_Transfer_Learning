9566f9a98a5e32d193cfd4e2186363cef9bc9fa1
Finallap <547805712@qq.com>
2020-06-21 13:26:26 +0800

    适配Widar3数据集

--- DAN/DANNet.py
+++ DAN/DANNet.py
@@ -45,16 +45,16 @@ class SharedNet(nn.Module):
             # conv1d
             # 输入的数据格式是（batch_size, word_vector, sequence_length）
             self.base_network = nn.Sequential(
-                nn.Conv1d(in_channels=180,out_channels=128, kernel_size=8, padding_mode='same'),
-                nn.BatchNorm1d(num_features = 128),
+                nn.Conv1d(in_channels=config['input_feature'], out_channels=128, kernel_size=8, padding_mode='circular'),
+                nn.BatchNorm1d(num_features=128),
                 nn.ReLU(),
-                nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5, padding_mode='same'),
-                nn.BatchNorm1d(num_features = 256),
+                nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5, padding_mode='circular'),
+                nn.BatchNorm1d(num_features=256),
                 nn.ReLU(),
-                nn.Conv1d(in_channels=256, out_channels=128, kernel_size=3, padding_mode='same'),
-                nn.BatchNorm1d(num_features = 128),
+                nn.Conv1d(in_channels=256, out_channels=128, kernel_size=3, padding_mode='circular'),
+                nn.BatchNorm1d(num_features=128),
                 nn.ReLU(),
-                nn.AdaptiveAvgPool1d(output_size = 1)
+                nn.AdaptiveAvgPool1d(output_size=1)
             )
         else:
             # conv
@@ -69,20 +69,21 @@ class SharedNet(nn.Module):
                 nn.MaxPool2d(kernel_size=2, stride=2),
                 nn.ReLU(),
             )
-            self.bottleneck = nn.Linear(config['batch_size'] * 3680, 128)
+            # self.bottleneck = nn.Linear(config['batch_size'] * 3680, 128)
+            self.bottleneck = nn.Linear(config['batch_size'] * 3968, 128)
 
     def forward(self, input):
         # lstm
         if self.config['model_type'] == 'lstm':
             x, _ = self.base_network(input)
-            output = self.bottleneck(x[:, -1, :])
+            x = self.bottleneck(x[:, -1, :])
         elif self.config['model_type'] == 'conv1d':
             # conv1d
             x = self.base_network(input)
-            output = x.view(self.config['batch_size'], -1)
+            x = x.view(self.config['batch_size'], -1)
         else:
             # conv
             x = self.base_network(input)
             x = x.view(self.config['batch_size'], -1)
-            output = self.bottleneck(x)
-        return output
+            x = self.bottleneck(x)
+        return x
